6eb14cfbe7ef1be3da2ff44e9fb6bad04c3d607e
yama-chiiii <nagayama0409@gmail.com>
2023-06-20 16:29:44 +0900

    first commit

--- src/ui/Question/index.tsx
+++ src/ui/Question/index.tsx
@@ -1,8 +1,10 @@
 export const QuestionPage = () => {
     return (
         <div className="bg-blue-deep flex justify-center">
-            <div className="flex flex-col items-center justify-center h-screen text-white">
-                クイズページ
+            <div className="flex flex-col items-center justify-center w-full h-screen text-white">
+                <div className="max-w-lg min-h-screen mx-auto px-24 flex flex-col justify-center items-center">
+                    <h1 className="mb-16 text-gray-light text-2xl">Question</h1>
+                </div>
             </div>
         </div>
     );
